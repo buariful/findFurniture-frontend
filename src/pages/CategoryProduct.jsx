1f42d83be5c03d698a44d60713cf6ff5b2c38e4f
ariful <arifulislam321054@gmail.com>
2023-08-14 22:12:00 +0600

    CategoryProduct component update

--- src/pages/CategoryProduct.jsx
+++ src/pages/CategoryProduct.jsx
@@ -3,30 +3,38 @@ import ProductSection from "../component/home/ProductSection";
 import { useDispatch, useSelector } from "react-redux";
 import DashboardTitle from "../component/shared/DashboardTitle";
 import { Breadcrumbs } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HomeIcon } from "@heroicons/react/24/outline";
 import { resetFilter } from "../features/searchFilter/searchFilterSlice";
 
 const CategoryProduct = () => {
   const category = useSelector((state) => state.filter.categories[0]);
   const dispatch = useDispatch();
+  const location = useLocation();
   return (
     <>
       <div className="py-5">
         <DashboardTitle text={category} />
       </div>
-      <div className="w-11/12 mx-auto">
-        <Breadcrumbs>
-          <Link
-            className="flex items-center gap-1 font-semibold"
-            to="/"
-            onClick={() => dispatch(resetFilter())}
-          >
-            <HomeIcon className="w-4" /> <span>Home</span>
-          </Link>
-          <p className="cursor-text capitalize hover:text-black">{category}</p>
-        </Breadcrumbs>
-      </div>
+      {location === "/category-product" ? (
+        <div className="w-11/12 mx-auto">
+          <Breadcrumbs>
+            <Link
+              className="flex items-center gap-1 font-semibold"
+              to="/"
+              onClick={() => dispatch(resetFilter())}
+            >
+              <HomeIcon className="w-4" /> <span>Home</span>
+            </Link>
+            <p className="cursor-text capitalize hover:text-black">
+              {category}
+            </p>
+          </Breadcrumbs>
+        </div>
+      ) : (
+        ""
+      )}
+
       <ProductSection />
     </>
   );
